e05985325bf69b68286907829083b803d2ad3dc6
Dharmesh Singh <dharmesh.singh@hcl.com>
2024-02-17 20:32:44 +0530

    Add Book Model and Migration

---
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_DataAccess.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=LP-5CG2262DTM;Database=CodingWiki;TrustServerCertificate=True;Trusted_Connection=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().Property(u => u.Price).HasPrecision(10, 5);
+        }
+    }
+}
