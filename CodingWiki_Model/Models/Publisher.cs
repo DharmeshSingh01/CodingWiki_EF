a40f11c192a10787ae2e48cc680313873a548768
Dharmesh Singh <dharmesh.singh@hcl.com>
2024-02-26 21:33:41 +0530

    Add Author and Publisher

---
+++ CodingWiki_Model/Models/Publisher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_Model.Models
+{
+    public class Publisher
+    {
+        public int Publisher_Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
