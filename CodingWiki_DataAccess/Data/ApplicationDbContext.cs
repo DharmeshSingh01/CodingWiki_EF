76f17306c211c8375f4af998dc37adef990fac32
Dharmesh Singh <dharmesh.singh@hcl.com>
2024-02-27 07:00:41 +0530

    Seed Data

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -26,6 +26,21 @@ namespace CodingWiki_DataAccess.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Book>().Property(u => u.Price).HasPrecision(10, 5);
+
+            var bookList = new Book[]{
+                new Book { BookId=1, Title="Spider Without Deuty",ISBN="123B12",Price=10.99m,Publisher_Id=1 },
+                new Book { BookId=2, Title="Fortune Of Time",ISBN="123B13",Price=9.99m,Publisher_Id = 2 },
+                new Book { BookId=3, Title="Fake Sunday",ISBN="123B14",Price=8.99m,Publisher_Id=1 },
+                new Book { BookId=4, Title="Cookies Jar",ISBN="123B15",Price=10.99m , Publisher_Id = 3},
+                new Book { BookId=5, Title="Spider Without Deuty-2",ISBN="123B16",Price=11.99m,Publisher_Id=1 }
+            };
+            modelBuilder.Entity<Book>().HasData(bookList);
+            
+            modelBuilder.Entity<Publisher>().HasData(
+                 new Publisher { Publisher_Id=1, Name="ABC"},
+                 new Publisher { Publisher_Id = 2, Name = "BPC" },
+                 new Publisher { Publisher_Id = 3, Name = "APress" }
+                );
         }
     }
 }
---
+++ CodingWiki_DataAccess/Migrations/20240227012956_SeedData.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CodingWiki_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Publishers",
+                columns: new[] { "Publisher_Id", "Location", "Name" },
+                values: new object[,]
+                {
+                    { 1, null, "ABC" },
+                    { 2, null, "BPC" },
+                    { 3, null, "APress" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "BookId", "ISBN", "Price", "Publisher_Id", "Title" },
+                values: new object[,]
+                {
+                    { 1, "123B12", 10.99m, 1, "Spider Without Deuty" },
+                    { 2, "123B13", 9.99m, 2, "Fortune Of Time" },
+                    { 3, "123B14", 8.99m, 1, "Fake Sunday" },
+                    { 4, "123B15", 10.99m, 3, "Cookies Jar" },
+                    { 5, "123B16", 11.99m, 1, "Spider Without Deuty-2" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Publisher_Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Publisher_Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Publisher_Id",
+                keyValue: 3);
+        }
+    }
+}
